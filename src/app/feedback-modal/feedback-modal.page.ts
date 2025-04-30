bc818a923f332519bf41297a6c3de74abf908b41
lemmadriaga <137263436+lemmadriaga@users.noreply.github.com>
2025-04-30 23:44:33 +0800

    report dark

--- src/app/feedback-modal/feedback-modal.page.ts
+++ src/app/feedback-modal/feedback-modal.page.ts
@@ -28,50 +28,50 @@ export class FeedbackModalPage {
   }
   async submitFeedback() {
     if (this.isSubmitting) return;
-  
+
     if (this.rating === 0) {
       const alert = await this.alertController.create({
         header: 'Rating Required',
         message: 'Please provide a rating before submitting your feedback.',
-        buttons: ['OK']
+        buttons: ['OK'],
       });
       await alert.present();
       return;
     }
-  
+
     this.isSubmitting = true;
-  
+
     try {
-      
       const user = await this.auth.currentUser;
       const userId = user ? user.uid : null;
-  
+
       if (userId) {
-        
-        const userDoc = await this.firestore.collection('users').doc(userId).get().toPromise();
+        const userDoc = await this.firestore
+          .collection('users')
+          .doc(userId)
+          .get()
+          .toPromise();
         const userData = userDoc.data();
-        const userName = (userData as { fullName?: string })?.fullName || 'Anonymous'; 
-  
+        const userName =
+          (userData as { fullName?: string })?.fullName || 'Anonymous';
+
         const feedbackData = {
           name: userName,
           comment: this.feedback,
           rating: this.rating,
           createdAt: new Date(),
         };
-  
-        
+
         await this.firestore.collection('feedback').add(feedbackData);
         console.log('Feedback successfully saved in Firestore');
-  
-        
+
         const thankYouAlert = await this.alertController.create({
           header: 'Thank You!',
           message: 'Your feedback has been submitted successfully.',
-          buttons: ['OK']
+          buttons: ['OK'],
         });
         await thankYouAlert.present();
-  
-        
+
         await thankYouAlert.onDidDismiss();
         this.feedback = '';
         this.rating = 0;
@@ -79,17 +79,13 @@ export class FeedbackModalPage {
       } else {
         console.error('No authenticated user found.');
       }
-      
     } catch (error) {
       console.error('Error saving feedback:', error);
     } finally {
       this.isSubmitting = false;
     }
   }
-  
-  
-  
-  
+
   close() {
     this.location.back();
   }
